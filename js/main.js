96b9b0a075d99386769195a467909c446106f06f
Wyndam Baxter <84729993+WyndamBaxter@users.noreply.github.com>
2021-08-12 01:20:17 -0300

    Aula 1 práticas

---
+++ js/main.js
@@ -0,0 +1,59 @@
+
+var d = new Date();
+alert(d.getMonth()+1);
+
+
+
+
+/*var count;
+for(count = 0; count<=5; count++ ){
+    alert(count);
+}*/
+
+
+/*var count = 0;
+while (count<=5){
+    console.log(count);
+    alert(count);
+    count++;
+};*/
+
+
+
+/*var idade = prompt("Qual sua idade?");
+if(idade >= 18){
+    alert("maior de idade");
+} else{
+    alert("Menor de idade");
+}*/
+
+//var frutas = [{nome:"maçâ", cor:"vermelha"},{nome:"banana", cor:"amarela"}];
+//console.log(frutas);
+//alert(frutas[1].nome);
+
+
+//var fruta = {nome:"maçâ", cor:"vermelha"};
+//console.log(fruta.nome);
+//alert(fruta.cor);
+
+
+//var lista = ["maçâ", "pêra", "laranja"];
+//lista.pop();
+//lista.push("uva");
+//console.log(lista);
+//console.log(lista.toString());
+//console.log(lista.join("-"));
+
+
+
+//var nome = "Wyndam Baxter Cardoso";
+//var n1 = 29;
+//var n2 = 10;
+//var frase = "Japão é o melhor time do mundo";
+//alert(nome + " tem " + idade +" anos");
+//alert(idade+idade2);
+//console.log(nome);
+//console.log(n1 * n2);
+//console.log(frase.toLocaleUpperCase());
+//alert(frase.replace("Japão", "Brasil"));
+
